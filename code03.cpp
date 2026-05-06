4456e82b398885442da3c2bf40f3fae5cc5580bf
SMSup10 <75311640+SMSup10@users.noreply.github.com>
2026-05-06 14:21:40 +0900

    Create code03.cpp

---
+++ code03.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+
+int main(int argc, char* argv[]) {
+
+	int a, b, c;
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = a + b;
+	std::cout << c;
+}
+
+-----------------------------------
+1. 개발자 명령 프롬포트 열기
+2. dir 입력
+3. cd project2
+4. FileName.cpp
+5. FileName.exe
+6. dir *.exe
+7.FileName 111 222입력
+8. 결과값 333 나오면 정답
